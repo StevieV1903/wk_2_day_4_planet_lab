33e156944542987be2a3fb12cd1313ecff0fb90f
Steve Vance <steve.vance@hotmail.co.uk>
2019-11-28 14:06:37 +0000

    passed test found planet by name

--- start_point/star_system.rb
+++ start_point/star_system.rb
@@ -7,8 +7,13 @@ class StarSystem
   end
 
   def planet_names()
-    planets_in_system =  @planets.map{ |planet| planet.name}
+    planets_in_system =  @planets.map { |planet| planet.name}
     return planets_in_system
+    end
+
+    def get_planet_by_name(planet_name)
+      found_planet = @planets.find { |planet| planet.name == planet_name}
+      return found_planet
     end
 
 
